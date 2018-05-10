5f5003ff8ead6a11559f73b0f7eb0f02fc8c22a9
Roman Makarchyk <messarro@gmail.com>
2018-05-10 11:43:23 +0300

    Finishing 5-th Chapter

--- collection/views.py
+++ collection/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    number = 6
+    thing = "Cosmic name"
+    return render(request, 'index.html', {
+        "number": number,
+        "thing": thing})
