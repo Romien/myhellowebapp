5f9922338e9046d44202c88e868b18719323156d
Roman Makarchyk <messarro@gmail.com>
2018-05-28 18:07:51 +0300

    Finish Chapter 10

--- collection/models.py
+++ collection/models.py
@@ -9,3 +9,6 @@ class Thing(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return u"%s %s" % (self.name, self.description)
--- collection/views.py
+++ collection/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from collection.forms import ThingForm
 from collection.models import Thing
 
 # Create your views here.
@@ -7,4 +9,41 @@ from collection.models import Thing
 def index(request):
     things = Thing.objects.all()
     return render(request, 'index.html', {
-        "things": things})
+        "things": things,
+    })
+# our new view:
+
+
+def thing_detail(request, king):
+    # grab the object...
+    thing = Thing.objects.get(slug=king)
+
+    # and pass to the temlplate
+    return render(request, 'things/thing_detail.html', {
+        'thing': thing,
+    })
+
+
+def edit_thing(request, king):
+    # grab the object...
+    thing = Thing.objects.get(slug=king)
+    # set the form we're using
+    form_class = ThingForm
+
+    # if we're coming to this view from a submitted form
+    if request.method == 'POST':
+        # grab the data from the submitted form and apply to the form
+        form = form_class(data=request.POST, instance=thing)
+        if form.is_valid():
+            # save the new data
+            form.save()
+            return redirect('thing_detail', king=thing.slug)
+    # otherwise just create the form
+    else:
+        form = form_class(instance=thing)
+
+    # and render the template
+    return render(request, 'things/edit_thing.html', {
+        'thing': thing,
+        'form': form,
+    })
